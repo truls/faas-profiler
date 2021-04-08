55d731f088ac87b19a08056aa359a61a503caf2f
Truls Asheim <truls@asheim.dk>
2021-04-08 14:31:50 +0200

    format

--- functions/ocr-img/handler.js
+++ functions/ocr-img/handler.js
@@ -5,28 +5,28 @@
  LICENSE file in the root directory of this source tree.
 */
 
-const tesseract = require('tesseractocr')
+const tesseract = require('tesseractocr');
 
 function main(params) {
-    text = new Promise((resolve, reject) => {
-        let text = tesseract.recognize(Buffer.from(params.__ow_body, "base64"), (err, text) => {
-	    if (err) {
-                var response = {
-                    statusCode: 500,
-                    body: "Error!"
-                };
-	        resolve(response);
-	    } else {
-                var response = {
-                    statusCode: 200,
-                    body: text
-                };
-	        resolve(response);
-	    }
-	});
+  text = new Promise((resolve, reject) => {
+    let text = tesseract.recognize(Buffer.from(params.__ow_body, "base64"), (err, text) => {
+      if (err) {
+        var response = {
+          statusCode: 500,
+          body: "Error!"
+        };
+        resolve(response);
+      } else {
+        var response = {
+          statusCode: 200,
+          body: text
+        };
+        resolve(response);
+      }
     });
-        
-    return text;
+  });
+
+  return text;
 }
 
 exports.main = main;
